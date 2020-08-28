e70cc4eb65460d24d6d531b47cd94a6441ace6ab
sarbar2002 <sxs6666@psu.edu>
2020-08-28 01:06:07 +0000

    aa

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("Hello World")
 name = input("Enter your name: ")
-print("Hello," + name + "!")
+print("Hello, " + name + "!")
 age = input("Enter your age: ")
 age = int(age)
 
-print("You will be " + str(age + 1) + " next year")
+print("You will be " + str(age + 1) + " next year.")
