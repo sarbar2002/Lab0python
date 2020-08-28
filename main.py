f761ea52bb2f5143fff00bb2f8db9c9b7fda2a09
sarbar2002 <sxs6666@psu.edu>
2020-08-28 00:54:20 +0000

    lab0-python

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello Word")
+print("Hello World")
+name = input("Enter your name ")
+print("Hello," + name + "!")
+age = input("Enter your age: ")
+age = int(age)
+
+print("You will be " + str(age + 1) + " next year")
